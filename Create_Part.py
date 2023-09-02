99475a2cd5b4b65ba8c30edc3f3dbe09a106dc56
furti <daniel.furtlehner@gmx.net>
2023-09-02 14:35:42 +0200

    Create script to add new part with 4 boards

---
+++ Create_Part.py
@@ -0,0 +1,72 @@
+import FreeCAD
+import FreeCADGui
+import os
+from pathlib import Path
+from PySide2 import QtWidgets
+from Platte_Erzeugen import create_board
+
+def createBody(doc, part, name):
+  #1. Create the folder that holds all boards
+  documentLocation = doc.FileName
+  boardFolder = Path(os.path.join(os.path.dirname(documentLocation), 'Teile', name))
+
+  if not boardFolder.is_dir():
+    boardFolder.mkdir(parents=True)
+  
+  # 2. Create the bottom board
+  bottomName = name + '_Boden'
+  bottomLocation = boardFolder.joinpath(bottomName + '.FCStd')
+  bottom = create_board(str(bottomLocation), bottomName, doc)
+  bottom.setExpression('.Placement.Base.x', u'Staerke')
+
+  # 2. Create the left board
+  leftName = name + '_Seitenwand_Links'
+  leftLocation = boardFolder.joinpath(leftName + '.FCStd')
+  left = create_board(str(leftLocation), leftName, doc)
+  left.setExpression('.Placement.Base.y', u'Breite')
+  left.Placement.Rotation = FreeCAD.Rotation(FreeCAD.Vector(0.707, 0.000, 0.707), 180.000)
+
+  # 2. Create the right board
+  rightName = name + '_Seitenwand_Rechts'
+  rightLocation = boardFolder.joinpath(rightName + '.FCStd')
+  right = create_board(str(rightLocation), rightName, doc)
+  right.setExpression('.Placement.Base.x', u'Staerke * 2 + ' + bottomName + '.Laenge')
+  right.Placement.Rotation = FreeCAD.Rotation(FreeCAD.Vector(0., 1.000, 0), 270.000)
+
+  # 2. Create the top board
+  topName = name + '_Deckel'
+  topLocation = boardFolder.joinpath(topName + '.FCStd')
+  top = create_board(str(topLocation), topName, doc)
+  top.setExpression('.Placement.Base.x', u'Staerke')
+  top.setExpression('.Placement.Base.z', leftName + '.Laenge - Staerke')
+
+  return None
+
+currentDoc = FreeCAD.ActiveDocument
+
+# 1. Document Location
+documentLocation = QtWidgets.QFileDialog.getSaveFileName(QtWidgets.QApplication.activeWindow(), 'Save as', '', 'Freecad Files (*.FCStd)')[0]
+name = os.path.splitext(os.path.basename(documentLocation))[0]
+
+# 2. Create Document and save it
+FreeCAD.newDocument(name)
+doc = FreeCAD.getDocument(name)
+guidoc = FreeCADGui.getDocument(name)
+doc.saveAs(documentLocation)
+
+# 3. Link masse spreadsheet
+doc.addObject('App::Link', 'Masse').setLink(FreeCAD.getDocument('Masse').Spreadsheet)
+doc.recompute(None,True,True)
+
+# 4. Create Part
+doc.addObject('App::Part', name)
+part = doc.getObject(name)
+part.Label = name
+doc.recompute()
+
+#5. Create Body
+createBody(doc, part, name)
+
+#6. Link Part
+currentDoc.addObject('App::Link', name).setLink(part)
+currentDoc.recompute(None,True,True)
---
+++ Platte_Erzeugen.py
@@ -0,0 +1,99 @@
+import FreeCAD
+import FreeCADGui
+import PartDesign
+import PartDesignGui
+import Sketcher
+import Part
+from PySide2 import QtWidgets
+import os
+
+def create_board(documentLocation, name, currentDoc):
+  # 2. Create Document and save it
+  FreeCAD.newDocument(name)
+  doc = FreeCAD.getDocument(name)
+  guidoc = FreeCADGui.getDocument(name)
+  doc.saveAs(documentLocation)
+
+  # 3. Link masse spreadsheet
+  doc.addObject('App::Link', 'Masse').setLink(FreeCAD.getDocument('Masse').Spreadsheet)
+  doc.recompute(None,True,True)
+
+  # 4. Create Object
+  doc.addObject('PartDesign::Body', name)
+  body = doc.getObject(name)
+  body.newObject('Sketcher::SketchObject', 'Sketch')
+  sketch = doc.getObject('Sketch')
+  sketch.Support = (doc.getObject('XY_Plane'),[''])
+  sketch.MapMode = 'FlatFace'
+  doc.recompute()
+
+  # 5. Customize Sketch
+  guidoc.setEdit(body,0,'Sketch.')
+
+  geoList = []
+  geoList.append(Part.LineSegment(FreeCAD.Vector(0,0,0), FreeCAD.Vector(1000,0,0)))
+  geoList.append(Part.LineSegment(FreeCAD.Vector(1000,0,0),FreeCAD.Vector(1000,1000,0)))
+  geoList.append(Part.LineSegment(FreeCAD.Vector(1000,1000,0),FreeCAD.Vector(0,1000,0)))
+  geoList.append(Part.LineSegment(FreeCAD.Vector(0,1000,0),FreeCAD.Vector(0,0,0)))
+  sketch.addGeometry(geoList,False)
+
+  conList = []
+  conList.append(Sketcher.Constraint('Coincident',0,2,1,1))
+  conList.append(Sketcher.Constraint('Coincident',1,2,2,1))
+  conList.append(Sketcher.Constraint('Coincident',2,2,3,1))
+  conList.append(Sketcher.Constraint('Coincident',3,2,0,1))
+  conList.append(Sketcher.Constraint('Horizontal',0))
+  conList.append(Sketcher.Constraint('Horizontal',2))
+  conList.append(Sketcher.Constraint('Vertical',1))
+  conList.append(Sketcher.Constraint('Vertical',3))
+  sketch.addConstraint(conList)
+
+  sketch.addConstraint(Sketcher.Constraint('Coincident',0,1,-1,1)) 
+  sketch.addConstraint(Sketcher.Constraint('DistanceX',0,1,0,2,56.837105)) 
+  sketch.setDatum(9,FreeCAD.Units.Quantity('1000 mm'))
+  sketch.renameConstraint(9, u'Laenge')
+  sketch.addConstraint(Sketcher.Constraint('DistanceY',1,1,1,2,42.152203)) 
+  sketch.setDatum(10,FreeCAD.Units.Quantity('1000 mm'))
+  sketch.renameConstraint(10, u'Breite')
+  guidoc.resetEdit()
+  doc.recompute()
+
+  # 6. Pad sketch
+  body.newObject('PartDesign::Pad', 'Pad')
+  pad = doc.getObject('Pad')
+  pad.Profile = sketch
+  pad.Length = 10.0
+
+  # Set general properties
+  body.addProperty("App::PropertyString", "Typ", "Info", "Typ")
+  body.addProperty("App::PropertyString", "Url", "Info", "Url")
+  body.addProperty("App::PropertyLength", "Breite", "Info", "Breite")
+  body.addProperty("App::PropertyLength", "Laenge", "Info", "Laenge")
+  body.addProperty("App::PropertyLength", "Staerke", "Info", "Staerke")
+
+  body.setExpression('Breite', u'Sketch.Constraints.Breite')
+  body.setExpression('Laenge', u'Sketch.Constraints.Laenge')
+  body.setExpression('Staerke', u'Pad.Length')
+
+  # Set drawing properties
+  body.addProperty("App::PropertyStringList", "Schnittliste_Ansichten", "Drawing", "Schnittliste_Ansichten")
+
+  body.ViewObject.DisplayModeBody = 'Tip'
+
+  doc.recompute()
+  sketch.Visibility = False
+  doc.recompute()
+
+  # Link into the parent doc
+  currentDoc.addObject('App::Link', name).setLink(body)
+  currentDoc.recompute(None,True,True)
+
+  return currentDoc.getObject(name)
+
+if __name__ == '__main__':
+  currentDoc = FreeCAD.ActiveDocument
+  # 1. Document Location
+  documentLocation = QtWidgets.QFileDialog.getSaveFileName(QtWidgets.QApplication.activeWindow(), 'Save as', '', 'Image Files (*.FCStd)')[0]
+  name = os.path.splitext(os.path.basename(documentLocation))[0]
+
+  create_board(documentLocation, name, currentDoc)
