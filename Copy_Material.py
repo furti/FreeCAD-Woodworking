d5940e7f7290914db4bc864916d3d6002de863bf
furti <daniel.furtlehner@gmx.net>
2024-03-02 14:45:28 +0100

    Add additional properties to objects

--- Copy_Material.py
+++ Copy_Material.py
@@ -1,19 +1,34 @@
 import FreeCAD
 import FreeCADGui
 
+def isLink(o):
+  return o.TypeId == 'App::Link'
+
+def getRoot(o):
+  root = o
+  
+  while isLink(root):
+   root = root.LinkedObject
+  
+  return root
+
 selection = FreeCADGui.Selection.getSelection()
-source_object = selection[0].LinkedObject
+source_object = getRoot(selection[0])
 source_view_object = source_object.ViewObject
 others = selection[1:]
 
-
-
 for o in others:
-  object = o.LinkedObject
+  object = getRoot(o)
+
+  if object == source_object:
+    continue
+
   view_object = object.ViewObject
   
   object.Typ = source_object.Typ
   object.Url = source_object.Url
+  object.Abrechnung_Art = source_object.Abrechnung_Art
+  object.Einzelpreis = source_object.Einzelpreis
 
   view_object.ShapeColor = source_view_object.ShapeColor
   view_object.Transparency = source_view_object.Transparency
--- Properties.py
+++ Properties.py
@@ -1,20 +1,40 @@
 import FreeCAD
 import FreeCADGui
 
+objects = FreeCADGui.Selection.getSelection()
 
-o = FreeCADGui.Selection.getSelection()[0]
+def isLink(o):
+  return o.TypeId == 'App::Link'
 
-if not hasattr(o, "Breite"):
-    o.addProperty("App::PropertyLength", "Breite", "Info", "Breite").Breite=1.0
-    o.setExpression('Breite', u'Sketch.Constraints.Breite')
+def getRoot(o):
+  root = o
+  
+  while isLink(root):
+   root = root.LinkedObject
+  
+  return root
 
-if not hasattr(o, "Laenge"):
-    o.addProperty("App::PropertyLength", "Laenge", "Info", "Laenge").Laenge=1.0
-    o.setExpression('Laenge', u'Sketch.Constraints.Laenge')
+for o in objects:
+    root_object = getRoot(o)
+    if not hasattr(root_object, "Breite"):
+        root_object.addProperty("App::PropertyLength", "Breite", "Info", "Breite").Breite=1.0
+        root_object.setExpression('Breite', u'Sketch.Constraints.Breite')
 
-if not hasattr(o, "Staerke"):
-    o.addProperty("App::PropertyLength", "Staerke", "Info", "Staerke").Staerke=1.0
-    o.setExpression('Staerke', u'Pad.Length')
+    if not hasattr(root_object, "Laenge"):
+        root_object.addProperty("App::PropertyLength", "Laenge", "Info", "Laenge").Laenge=1.0
+        root_object.setExpression('Laenge', u'Sketch.Constraints.Laenge')
 
-if not hasattr(o, "Schnittliste_Ansichten"):
-    o.addProperty("App::PropertyStringList", "Schnittliste_Ansichten", "Drawing", "Schnittliste_Ansichten")
+    if not hasattr(root_object, "Staerke"):
+        root_object.addProperty("App::PropertyLength", "Staerke", "Info", "Staerke").Staerke=1.0
+        root_object.setExpression('Staerke', u'Pad.Length')
+
+    if not hasattr(root_object, "Schnittliste_Ansichten"):
+        root_object.addProperty("App::PropertyStringList", "Schnittliste_Ansichten", "Drawing", "Schnittliste_Ansichten")
+    
+    if not hasattr(root_object, "Abrechnung_Art"):
+        root_object.addProperty("App::PropertyEnumeration", "Abrechnung_Art", "Info", "Abrechnung_Art")
+        root_object.Abrechnung_Art = ["lfm", "m2", "m3"]
+        root_object.Abrechnung_Art = "m2"
+    
+    if not hasattr(root_object, "Einzelpreis"):
+        root_object.addProperty("App::PropertyFloat", "Einzelpreis", "Info", "Einzelpreis")
